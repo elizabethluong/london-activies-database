907187acc49760e3e9da41a9c75068f084ac1a11
Elizabeth luong <elizabethluong@live.co.uk>
2020-01-14 15:22:53 +0000

    incremental update

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const knex = require("knex");
 const app = express();
 
 // Parse requests into json format.
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.json({message: "Insert homepage message here"});
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Insert homepage message here" });
+});
+
+app.get("/api/v1/activities_info", (req, res) => {
+  // res.send("hello");
+  knex("activities_info")
+    .select("*")
+    .from("activities_info")
+    .then(result => {
+      res.json(response);
+    });
+});
+
+app.get("/test", (req, res) => {
+  knex("reviews").insert({ review: "Horrible!", activity_id: "1" });
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-})
+  console.log("Server is running on port 3000!");
+});
